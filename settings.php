488f9a7912536057738ec1b59fbb158694485f64
Hemstreet <jonhemstreet@gmail.com>
2015-03-20 23:45:20 -0600

    dev setup

---
+++ settings.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <title>Simply Social</title>
+    <script src="/public/js/jquery-1.11.1.min.js"></script>
+    <!--    <script src="/static/js/simplysocial.js"></script>-->
+    <link rel="stylesheet" href="/public/css/simplysocial.css">
+</head>
+<body>
+    <?php include('views/header.php'); ?>
+    Settings
+    <?php include('views/footer.php'); ?>
+</body>
+</html>
